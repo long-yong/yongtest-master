7d4b53771599e40efdf25d2d2f013785e3096fd2
long-yong <long.yong@outlook.com>
2018-09-27 15:58:10 -0700

    init

---
+++ src/test/java/com/coding/yongtest/YongtestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coding.yongtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YongtestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
